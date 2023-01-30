2500f9039f79641c2cbf45d530fb5efdc55da845
Javier Pera <javier.pera96@gmail.com>
2023-01-30 12:42:24 -0300

    2/3 secciones terminadas

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ tabs.forEach(tab =>{
         tab.classList.add('filter-tab-active')
     })
 })
+
+/**DARK-LIGHT THEME */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'theme--dark';
+const iconTheme = 'ri-sun-line';
+
+//Local Storage
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+//Obtener el modo actual
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+//Validacion para saber si el usuario escogio previamente un tema
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+//Activar y Desactivar el tema
+themeButton.addEventListener('click', () =>{
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
